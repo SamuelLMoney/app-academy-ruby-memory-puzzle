448ff46352c742552a21a795ffdcf7bcccd4e621
Kevin Cornea <kpcornea@gmail.com>
2018-11-19 22:39:10 -0800

    Board#won? and Board#reveal are working

--- board.rb
+++ board.rb
@@ -11,7 +11,7 @@ class Board
   attr_reader :grid
 
   def initialize
-    @grid = Array.new(4) { Array.new(4, :N) }
+    @grid = Array.new(4) { Array.new(4) }
     @hidden_grid = [] # nec?
   end
 
@@ -22,8 +22,8 @@ class Board
     first_half = Array.new(4 * 4 / 2) { Card.new } # remember to require other classes
     # first_half = Array.new(8) { Card.new }
     # byebug
-    @grid.map! do |sub_arr|
-      sub_arr.map! do |ele|
+    @grid.map! do |row|
+      row.map! do |ele|
         # new_card = rand(1..10)
         if first_half.empty? # i think works but not truly random. can never get completed pair before going through first_half. actually even worse than that, this way makes 2nd half the same order as first half. bad. this works now but still not pefect randomness.
           used.sample!
@@ -41,9 +41,9 @@ class Board
   def render
     # p @grid # no i need to convert the card objects into an understandable output and take into account faceup/down
 
-    @grid.each do |sub_arr|
+    @grid.each do |row|
       displayed_row = []
-      sub_arr.each do |card|
+      row.each do |card|
         if card.face_down
           displayed_row << "?"
         else
@@ -55,11 +55,23 @@ class Board
   end
 
   def won?
-
+    @grid.all? do |row|
+      row.all? do |card|
+        !card.face_down
+      end
+    end
   end
 
-  def reveal
-
+  def reveal(guessed_pos) # input in form "0 0"
+    # byebug
+    i, j = guessed_pos[0].to_i, guessed_pos[-1].to_i
+    guessed_card = @grid[i][j]
+    # byebug
+    if guessed_card.face_down
+      # byebug
+      guessed_card.reveal
+      return guessed_card.display
+    end
   end
 
 
